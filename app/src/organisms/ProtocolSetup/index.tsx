3c7e4db6ba70fa26b1caca6ea27927f2623e293e
Jethary Rader <jethary@opentrons.com>
2021-11-18 11:21:02 -0500

    feat(app): add LPC Success Toast
    closes #8486

--- app/src/organisms/ProtocolSetup/index.tsx
+++ app/src/organisms/ProtocolSetup/index.tsx
@@ -1,14 +1,43 @@
 import * as React from 'react'
-
-import { DIRECTION_COLUMN, SPACING_3, Flex } from '@opentrons/components'
+import {
+  DIRECTION_COLUMN,
+  SPACING_3,
+  Flex,
+  AlertItem,
+  SPACING_1,
+  SPACING_2,
+} from '@opentrons/components'
 import { RunSetupCard } from './RunSetupCard'
 import { MetadataCard } from './MetadataCard'
+import { useTranslation } from 'react-i18next'
 
 export function ProtocolSetup(): JSX.Element {
+  const { t } = useTranslation('protocol_info')
+  const [dismissed, setDismissed] = React.useState(false)
+
   return (
-    <Flex flexDirection={DIRECTION_COLUMN} padding={SPACING_3}>
-      <MetadataCard />
-      <RunSetupCard />
-    </Flex>
+    <>
+      <Flex
+        flexDirection={DIRECTION_COLUMN}
+        padding={`${SPACING_1} ${SPACING_2} ${SPACING_1} ${SPACING_2}`}
+      >
+        {!dismissed && (
+          <AlertItem
+            type="success"
+            onCloseClick={() => setDismissed(true)}
+            title={t('labware_positon_check_complete_toast', {
+              num_offsets: 2, //  TODO wire up num_offsets!
+            })}
+          />
+        )}
+      </Flex>
+      <Flex
+        flexDirection={DIRECTION_COLUMN}
+        padding={`${SPACING_1} ${SPACING_3} ${SPACING_3} ${SPACING_3}`}
+      >
+        <MetadataCard />
+        <RunSetupCard />
+      </Flex>
+    </>
   )
 }
